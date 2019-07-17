5cca8358b4623075d85db9bdacdcec8ed79f5fd3
Avi Barel <avraham.barel@here.com>
2019-07-17 18:53:22 +0300

    Added Toolbar Back Press

--- app/src/main/java/com/example/smartride/widgets/MainToolBar.kt
+++ app/src/main/java/com/example/smartride/widgets/MainToolBar.kt
@@ -2,8 +2,10 @@ package com.example.smartride.widgets
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import com.example.smartride.R
+import com.example.smartride.base.IToolBar
 import com.google.android.material.appbar.AppBarLayout
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.toolbar_main.view.*
@@ -24,6 +26,25 @@ class MainToolBar : AppBarLayout {
         textTitle.text = context.getString(R.string.hi_user_toolbar, FirebaseAuth.getInstance().currentUser?.displayName ?: "User")
 
         updateToolBarMode()
+
+        imageBack.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN,
+                MotionEvent.ACTION_MOVE -> {
+                    v.alpha = 0.5f
+                }
+                else -> {
+                    v.alpha = 1f
+                }
+            }
+
+            return@setOnTouchListener false
+        }
+        imageBack.setOnClickListener {
+            (context as? IToolBar)?.let {
+                it.onToolBarBackPress()
+            }
+        }
     }
 
     override fun onDetachedFromWindow() {
