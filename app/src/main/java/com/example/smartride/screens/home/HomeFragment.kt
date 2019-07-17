8a013354f749df01fb1fdcd9b003a997af46de85
Yamin <Yamin.elmakis@here.com>
2019-07-17 18:02:32 +0300

    home screen update
    add home scene :)
    add animation to open explanation

--- app/src/main/java/com/example/smartride/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/smartride/screens/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.smartride.R
 import kotlinx.android.synthetic.main.fragment_home.*
+import lib.yamin.easylog.EasyLog
 
 class HomeFragment : Fragment() {
 
@@ -16,8 +17,14 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        pendingTimer.setTime(System.currentTimeMillis() + 75 * 1000)
         pendingStartInfo.setOnClickListener {
+            EasyLog.e("open")
             pendingRideLayout.transitionToState(R.id.explanation_open)
+        }
+        pendingExplanationInfo.setOnClickListener {
+            EasyLog.e("close")
+            pendingRideLayout.transitionToState(R.id.explanation_close)
         }
     }
 }
